36cfbb32a6c8e86accf9fe7b8685d108c5193b61
Haris Isani <haris.isani@clientpoint.net>
2023-04-19 13:50:18 -0700

    opt loader

--- js/main.js
+++ js/main.js
@@ -10,7 +10,9 @@ Author URI: http://themeforest.net/user/ingridk
 	
 	//Window load function
     $(window).load(function() {
-		
+			//Preloader		
+			$("#preloader").fadeOut("slow");
+			$("#spinner").fadeOut("slow");
 		//Open street  Map
 		var mapcanvas = document.getElementById("map-canvas");
 			if(mapcanvas){
@@ -37,9 +39,7 @@ Author URI: http://themeforest.net/user/ingridk
 			var marker = L.marker(coord, {icon: customIcon}).addTo(map);		
 		}
 		
-		//Preloader		
-		$("#preloader").fadeOut("slow");
-		// $("#spinner").fadeOut("slow");
+	
 		
 
 	//Portfolio Isotope Filter
