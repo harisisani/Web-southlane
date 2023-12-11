62006285fcd53d2ef9fad4980586e5d6ade86603
Haris Isani <55500840+harisisani@users.noreply.github.com>
2023-12-10 17:38:45 -0800

    fixed email

--- south-lane/api/email/sendmail.php
+++ south-lane/api/email/sendmail.php
@@ -14,17 +14,33 @@ if($_POST){
     try {
         // Server settings
         // $mail->SMTPDebug = SMTP::DEBUG_SERVER; // for detailed debug output
+        // $mail->isSMTP();
+        // $mail->Host = 'localhost';
+        // $mail->SMTPAuth = false;
+        // $mail->SMTPAutoTLS = false;
+        // $mail->Port = 25;
+    
+        // // $mail->Username = 'profilewebsite162@gmail.com'; // YOUR gmail email
+        // // $mail->Password = 'Programmer12345'; // YOUR gmail password
+    
+        // $mail->Username = 'no-reply@southlaneanimalhospital.com'; // YOUR gmail email
+        // $mail->Password = 'Programmer@123'; // YOUR gmail password
+
+
         $mail->isSMTP();
-        $mail->Host = 'localhost';
-        $mail->SMTPAuth = false;
+        $mail->Host = 'smtp.titan.email';
+        $mail->SMTPAuth = true;
         $mail->SMTPAutoTLS = false;
-        $mail->Port = 25;
-    
-        // $mail->Username = 'profilewebsite162@gmail.com'; // YOUR gmail email
-        // $mail->Password = 'Programmer12345'; // YOUR gmail password
-    
+        $mail->Port = 587; // You can also use 465 for SSL/TLS (deprecated) or 587 for STARTTLS
+
+        // Set your username and password for SMTP authentication
         $mail->Username = 'no-reply@southlaneanimalhospital.com'; // YOUR gmail email
-        $mail->Password = 'Programmer@123'; // YOUR gmail password
+        $mail->Password = 'Programmer@123'; 
+
+        // Enable SSL/TLS encryption
+        $mail->SMTPSecure = 'tls'; // You can also use 'ssl' (deprecated)
+
+
 
         // Sender and recipient settings
         $mail->setFrom('no-reply@southlaneanimalhospital.com', 'South Lane Animal Hospital');
