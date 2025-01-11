d52b5a6d5a515acf9050bf5bb07b67fc7a1c0827
Haris Isani <55500840+harisisani@users.noreply.github.com>
2025-01-11 06:20:22 -0800

    fix db

--- south-lane/api/database.php
+++ south-lane/api/database.php
@@ -2,12 +2,12 @@
     // session_start();
     //  if(isset($_SESSION["user_name"])){
          $host="localhost";
-         $db_name="app_southlane";
-         $user="root";
-         $password='';
-            // $db_name="u270102017_app_southlane";
-            // $user="u270102017_blitz";
-            // $password='Programmer@123';
+        //  $db_name="app_southlane";
+        //  $user="root";
+        //  $password='';
+            $db_name="u270102017_app_southlane";
+            $user="u270102017_blitz";
+            $password='Programmer@123';
             try{
                 $connection=new PDO("mysql:host=".$host.";dbname=".$db_name,$user,$password);
             }
